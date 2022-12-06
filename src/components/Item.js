7ed8a721e98486e4c31fadcb8d168ffbff3a2a68
Martin <souzam@sonic.net>
2022-12-06 17:19:58 -0500

    Ongoing construction

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,16 @@
 const Item = ({item}) => {
+
+    const handleClick = () => {
+        console.log("OINK")
+    }
+
     return (
         <div>
             <h3>{item.name}</h3>
             <span>${item.price}</span>
             <img src={item.image} />
             <p>{item.description}</p>
+            <button onClick={handleClick}>Add to Cart</button>
         </div>
     )
 }
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,7 @@
 const Order = ({order}) => {
     return(
         <div>
-            <h3>Order No.</h3>
+            <h3>Order No. {order.order_number}</h3>
                 <ul>
                     <li>Item</li>
                 </ul>
